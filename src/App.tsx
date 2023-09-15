f833989c0602658c290e2167ed638bc6152eb28f
charltonator <charltonlucian@gmail.com>
2023-09-15 16:14:07 +0800

    homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container } from "@mui/material";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import './App.css';
 
 export default function App() {
   return (
-    <>
+    <div className="App">
       <Navbar />
-      <Container>hello world!</Container>
-    </>
+      <Home/>
+    </div>
   );
 }
