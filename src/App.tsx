d78a9a9a99c050d35b6035f05e073eb1ec667255
LAPTOP-TCOCQJDD\Khor Jun Wei <khorjunweiwei@gmail.com>
2023-09-15 15:30:41 +0800

    add question list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Container, Stack } from "@mui/material";
 import Navbar from "./components/Navbar";
+import Titlebar from "./components/Questions/Titlebar";
 
 export default function App() {
   return (
     <>
+    <Stack gap={5}>
       <Navbar />
+      <Titlebar />
       <Container>hello world!</Container>
+    </Stack>
     </>
   );
 }
