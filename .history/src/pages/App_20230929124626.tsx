fe682418eef829b1c637a7df21ea606d4bf0cfdd
charltonator <charltonlucian@gmail.com>
2023-09-29 12:54:11 +0800

    re install

--- .history/src/pages/App_20230929124626.tsx
+++ .history/src/pages/App_20230929124626.tsx
@@ -2,9 +2,9 @@ import { Box, CssBaseline } from "@mui/material";
 import Navbar from "../components/Navbar";
 import Home from "../components/Home";
 import "./App.css";
+import Titlebar from "../components/Questions/QuestionsTable";
 import CenteredContainer from "../components/CenteredContainer";
 import ProblemSolver from "./ProblemSolver";
-import QuestionsTable from "../components/Questions/QuestionsTable";
 
 export default function App() {
   return (
--- .history/src/pages/App_20230929124637.tsx
+++ .history/src/pages/App_20230929124637.tsx
@@ -2,6 +2,7 @@ import { Box, CssBaseline } from "@mui/material";
 import Navbar from "../components/Navbar";
 import Home from "../components/Home";
 import "./App.css";
+import Titlebar from "../components/Questions/QuestionsTable";
 import CenteredContainer from "../components/CenteredContainer";
 import ProblemSolver from "./ProblemSolver";
 import QuestionsTable from "../components/Questions/QuestionsTable";
