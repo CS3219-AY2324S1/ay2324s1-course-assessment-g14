a905e6e254acf90403fbc2b5bce586e978cce972
Fong Mun Kit, Wilbur <wilbur.fongthemasterchief@gmail.com>
2023-10-23 20:28:29 +0800

    Add new dropdown table

--- frontend/src/components/Questions/QuestionsTable.tsx
+++ frontend/src/components/Questions/QuestionsTable.tsx
@@ -1,5 +1,3 @@
-// InterviewQuestionsTable.tsx
-
 import React, { useEffect, useState } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Select, MenuItem, TablePagination, SelectChangeEvent } from '@mui/material';
 import { useData } from "../../data/data.context";
@@ -8,99 +6,124 @@ interface Question {
   title: string;
   tags: string[];
   categories: string[];
-  //constraints: string[];
+  constraints: string[];
   difficulty: string;
   //description: string;
 }
 
-const ITEMS_PER_PAGE_OPTIONS = [5, 10]; // Number of items to display per page
+const ITEMS_PER_PAGE_OPTIONS = [5, 10];
 
 const InterviewQuestionsTable: React.FC = () => {
-  const [questionsData, setQuestions] = useState<Question[]>([]);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [itemsPerPage, setItemsPerPage] = useState<number>(ITEMS_PER_PAGE_OPTIONS[0]);
-  const { questions, getQuestions } = useData();
+    const [questionsData, setQuestions] = useState<Question[]>([]);
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [itemsPerPage, setItemsPerPage] = useState<number>(ITEMS_PER_PAGE_OPTIONS[0]);
+    const { questions, getQuestions } = useData();
 
-  useEffect(() => {
-    async function getInterviewQuestions() {
-      getQuestions();
-    }
-    getInterviewQuestions();
-    // console.log("here");
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    useEffect(() => {
+        async function getInterviewQuestions() {
+            getQuestions();
+        }
+        getInterviewQuestions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  useEffect(() => {
-    setQuestions(questions);
-    //console.log(questionsData);
-  }, [questions]);
+    useEffect(() => {
+        setQuestions(questions);
+    }, [questions]);
 
-  const handlePageChange = (event: unknown, newPage: number) => {
-    setCurrentPage(newPage);
-  };
+    const handleCategoriesChange = (event: SelectChangeEvent<unknown>) => {
+        setSelectedCategories(event.target.value as string[]);
+    };
 
-  const handleChangeItemsPerPage = (
-    event: SelectChangeEvent<unknown>,
-  ) => {
-    setItemsPerPage(event.target.value as number);
-    setCurrentPage(1);
-  };
+    const uniqueCategories = Array.from(new Set(questionsData.flatMap(question => question.categories)));
 
-  const indexOfLastQuestion = currentPage * itemsPerPage;
-  const indexOfFirstQuestion = indexOfLastQuestion - itemsPerPage;
-  const currentQuestions = questionsData.slice(indexOfFirstQuestion, indexOfLastQuestion);
+    const filteredQuestions = questionsData.filter(question => 
+        selectedCategories.length === 0 || 
+        selectedCategories.some(cat => question.categories.includes(cat))
+    );
 
-  return (
-    <><div style={{ maxHeight: '300px', overflowY: 'auto', width: '80%' }}>
-          <TableContainer component={Paper} style={{ margin: '10px', padding: '10px' }}>
-              <Table style={{ minWidth: 650, fontSize: '14px' }}>
-                  <TableHead>
-                      <TableRow >
-                          <TableCell>Title</TableCell>
-                          <TableCell>Tags</TableCell>
-                          <TableCell>Categories</TableCell>
-                          {/* <TableCell>Constraints</TableCell> */}
-                          <TableCell>Difficulty</TableCell>
-                          {/* <TableCell>Description</TableCell> */}
-                      </TableRow>
-                  </TableHead>
-                  <TableBody>
-                      {currentQuestions.map((question:Question, index:number) => (
-                          <TableRow key={index}>
-                              <TableCell>{question.title}</TableCell>
-                              <TableCell>{question.tags.join(', ')}</TableCell>
-                              <TableCell>{question.categories.join(', ')}</TableCell>
-                              {/* <TableCell>{question.constraints.join(', ')}</TableCell> */}
-                              <TableCell>{question.difficulty}</TableCell>
-                              {/* <TableCell>{question.description}</TableCell> */}
-                          </TableRow>
-                      ))}
-                  </TableBody>
-              </Table>
-          </TableContainer>
-      </div><div style={{ display: 'flex', alignItems: 'center' }}>
-              <Select
-                  value={itemsPerPage}
-                  onChange={handleChangeItemsPerPage}
-                  style={{ marginTop: '10px' }}
-              >
-                  {ITEMS_PER_PAGE_OPTIONS.map((option) => (
-                      <MenuItem key={option} value={option}>
-                          {`${option} per page`}
-                      </MenuItem>
-                  ))}
-              </Select>
+    const handlePageChange = (event: unknown, newPage: number) => {
+        setCurrentPage(newPage);
+    };
 
-              <TablePagination
-                  rowsPerPageOptions={[]}
-                  component="div"
-                  count={questionsData.length}
-                  rowsPerPage={itemsPerPage}
-                  page={currentPage - 1}
-                  onPageChange={handlePageChange} />
-          </div></>
-    
-  );
+    const handleChangeItemsPerPage = (
+        event: SelectChangeEvent<unknown>,
+    ) => {
+        setItemsPerPage(event.target.value as number);
+        setCurrentPage(1);
+    };
+
+    const indexOfLastQuestion = currentPage * itemsPerPage;
+    const indexOfFirstQuestion = indexOfLastQuestion - itemsPerPage;
+    const currentQuestions = filteredQuestions.slice(indexOfFirstQuestion, indexOfLastQuestion);
+
+    return (
+        <>
+            {/* Category Filter */}
+            <Select
+                multiple
+                value={selectedCategories}
+                onChange={handleCategoriesChange}
+                renderValue={(selected) => (selected as string[]).join(', ')}
+                style={{ marginTop: '10px', marginBottom: '10px', width: '80%' }}
+            >
+                {uniqueCategories.map((category) => (
+                    <MenuItem key={category} value={category}>
+                        {category}
+                    </MenuItem>
+                ))}
+            </Select>
+
+            <div style={{ maxHeight: '300px', overflowY: 'auto', width: '80%' }}>
+                <TableContainer component={Paper} style={{ margin: '10px', padding: '10px' }}>
+                    <Table style={{ minWidth: 650, fontSize: '14px' }}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Title</TableCell>
+                                <TableCell>Tags</TableCell>
+                                <TableCell>Categories</TableCell>
+                                <TableCell>Constraints</TableCell>
+                                <TableCell>Difficulty</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {currentQuestions.map((question: Question, index: number) => (
+                                <TableRow key={index}>
+                                    <TableCell>{question.title}</TableCell>
+                                    <TableCell>{question.tags.join(', ')}</TableCell>
+                                    <TableCell>{question.categories.join(', ')}</TableCell>
+                                    <TableCell>{question.constraints.join(', ')}</TableCell>
+                                    <TableCell>{question.difficulty}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Select
+                    value={itemsPerPage}
+                    onChange={handleChangeItemsPerPage}
+                    style={{ marginTop: '10px' }}
+                >
+                    {ITEMS_PER_PAGE_OPTIONS.map((option) => (
+                        <MenuItem key={option} value={option}>
+                            {`${option} per page`}
+                        </MenuItem>
+                    ))}
+                </Select>
+
+                <TablePagination
+                    rowsPerPageOptions={[]}
+                    component="div"
+                    count={filteredQuestions.length}
+                    rowsPerPage={itemsPerPage}
+                    page={currentPage - 1}
+                    onPageChange={handlePageChange} />
+            </div>
+        </>
+    );
 };
 
 export default InterviewQuestionsTable;
