943e4215ccb484d10e0d39ca9994ffcbf9880896
ngjiayuan <ngjiayuan.z@gmail.com>
2023-10-25 11:15:40 +0800

    fix docker

--- ApiGatewayService/src/index.ts
+++ ApiGatewayService/src/index.ts
@@ -18,64 +18,21 @@ app.use(cors());
 //   });
 
 // Define routes for each backend service for local development
-// app.use('/questionservice', (req, res) => {
-//     proxy.web(req, res, { target: 'http://localhost:3002' });
-// });
-
-// app.use('/authservice', (req, res) => {
-//     proxy.web(req, res, { target: 'http://localhost:3003' });
-// });
-
-// app.use('/userservice', (req, res) => {
-//     proxy.web(req, res, { target: 'http://localhost:3004' });
-// });
-
-// app.use('/matchingservice', (req, res) => {
-//     proxy.web(req, res, { target: 'http://localhost:3005' });
-// });
-
-//Define routes for each backend service for production/docker containers
 app.use("/questionservice", (req, res) => {
-  proxy.web(req, res, { target: "http://localhost:3002" });
-  //http://question-service:3002
-  //http://localhost:3002
+  proxy.web(req, res, { target: "http://question-service:3002" });
 });
 
 app.use("/authservice", (req, res) => {
-  proxy.web(req, res, { target: "http://localhost:3003" });
-  //http://auth-service:3003
-  //http://localhost:3003
+  proxy.web(req, res, { target: "http://auth-service:3003" });
 });
 
 app.use("/userservice", (req, res) => {
-  proxy.web(req, res, { target: "http://localhost:3004" });
-  // http://user-service:3004
-  //http://localhost:3004
+  proxy.web(req, res, { target: "http://user-service:3004" });
 });
 
 app.use("/matchingservice", (req, res) => {
-  proxy.web(req, res, { target: "http://localhost:3005" });
-  //http://matching-service:3005
-  //http://localhost:3005
+  proxy.web(req, res, { target: "http://matching-service:3005" });
 });
-
-//Define routes for each backend service for production/docker containers
-// app.use("/questionservice", (req, res) => {
-//   proxy.web(req, res, { target: "http://question-service:3002" });
-// });
-
-// app.use("/authservice", (req, res) => {
-//   proxy.web(req, res, { target: "http://auth-service:3003" });
-// });
-
-// app.use("/userservice", (req, res) => {
-//   proxy.web(req, res, { target: "http://user-service:3004" });
-// });
-
-// app.use("/matchingservice", (req, res) => {
-//   proxy.web(req, res, { target: "http://matching-service:3005" });
-// });
-
 
 // Start the server
 app.listen(3001, () => {
