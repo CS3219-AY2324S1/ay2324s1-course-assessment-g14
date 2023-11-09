5014312cf1e22ba0f0bff5fed4ed2edc4440eaca
Charlton Tan <charltonlucian@gmail.com>
2023-11-09 16:07:21 +0800

    modal for question table and auto refresh when edit question

--- frontend/src/components/ProblemSolverLeft.tsx
+++ frontend/src/components/ProblemSolverLeft.tsx
@@ -40,7 +40,7 @@ const ProblemSolverLeft = () => {
 }
 
   return (
-    <Paper elevation={3} sx={{ flex: 1, display: 'flex', flexDirection: 'column', padding: 2 }}>
+    <Paper elevation={3} sx={{ flex: 1, display: 'flex', flexDirection: 'column', padding: 2}}>
       <Typography variant="h4" gutterBottom sx={{ fontSize: '24px' }}>
         {question.title}
       </Typography>
--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -15,8 +15,6 @@ export default function App() {
       <CenteredContainer>
         <Home />
         <QuestionsTable />
-        <AddQuestionTab />
-        <EditQuestionsTab />
       </CenteredContainer>
     </Box>
   );
