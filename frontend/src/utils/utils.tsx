0e9b69cb0fa451937c55705c56e6385ce50a36e8
Charlton Tan <charltonlucian@gmail.com>
2023-11-14 15:36:41 +0800

    Update utils.tsx

--- frontend/src/utils/utils.tsx
+++ frontend/src/utils/utils.tsx
@@ -1,12 +1,28 @@
 import {decode} from "html-entities";
 import htmr from "htmr";
 import {ReactNode} from "react";
-import DOMPurify from "dompurify";
 
 export function parseHtmlDescription(description: string): ReactNode {
   // Decode escaped HTML characters and add text wrap to pre tags in the question description
+  try {
   let decodedDescription = decode(description)
     .replace(/<pre>/g, "<pre style=\"white-space: pre-wrap;\">");
-  decodedDescription = DOMPurify.sanitize(decodedDescription);
   return htmr(decodedDescription);
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export function parseHtmlDescriptionWithoutExamples(description: string): ReactNode {
+  // Decode escaped HTML characters and add text wrap to pre tags in the question description
+  try {
+  let decodedDescription = decode(description)
+    .replace(/<pre>/g, "<pre style=\"white-space: pre-wrap;\">");
+  const exampleIndex = decodedDescription?.indexOf("Example");
+
+  const newDescription = exampleIndex !== -1 ? decodedDescription?.substring(0, exampleIndex) : decodedDescription;
+  return htmr(newDescription);
+  } catch(e) {
+    console.log(e)
+  }
 }
